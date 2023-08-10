b44ad0b9d8e9c4fc8277bcb714a3a638da8a36b6
Gianmaria Del Monte <g.macmount@gmail.com>
2023-08-10 14:34:22 +0200

    fixes for going to prod

--- src/settings.js
+++ src/settings.js
@@ -1,5 +1,5 @@
 export default settings = {
   revaVersion: '7c8eb90',
-  dataUrl: 'https://api.jsonbin.io/v3/b/64ccc966b89b1e2299cb2afb',
-  dataTransform: d => d.record,
+  dataUrl: '/plugins',
+  dataTransform: d => d,
 };
