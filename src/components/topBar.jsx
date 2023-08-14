7fd8a1896606d430f0ca09605d7d858888070071
Javier Ferrer <javier.ferrer@cern.ch>
2023-08-14 18:00:42 +0200

    Disable build button while working

--- src/components/topBar.jsx
+++ src/components/topBar.jsx
@@ -1,11 +1,53 @@
 import { h } from 'preact';
+import { useEffect, useRef, useState } from 'preact/hooks';
 
 import { createBuildUrl } from '../utils';
 
+import Loading from '../assets/loading.svg';
 import platforms from '../assets/platforms.json';
 
 function TopBar({ platform, modules, platformHandler }) {
+  const [downloading, setDownloading] = useState(false);
   const buildUrl = createBuildUrl(platform, modules);
+  const aRef = useRef();
+
+  const handleBuildClick = e => {
+    e.preventDefault();
+    setDownloading(true);
+  };
+
+  useEffect(() => {
+    const prepareAndFetchBuild = async () => {
+      let res;
+
+      try {
+        res = await fetch(buildUrl);
+      } catch (err) {
+        alert('Something went wrong, try again later.');
+        setDownloading(false);
+      }
+
+      if (res.status > 199 && res.status < 300) {
+        const filename = res.headers.get('content-disposition').match(/"(?<filename>.*?)"/)
+          .groups.filename;
+        const blob = await res.blob();
+        const url = window.URL.createObjectURL(blob);
+
+        aRef.current.href = url;
+        aRef.current.download = filename;
+        aRef.current.click();
+      } else {
+        alert('Something went wrong, try again later.');
+        setDownloading(false);
+      }
+
+      setDownloading(false);
+    };
+
+    if (downloading) {
+      prepareAndFetchBuild();
+    }
+  }, [downloading]);
 
   return (
     <nav className="topbar">
@@ -20,7 +62,10 @@ function TopBar({ platform, modules, platformHandler }) {
         </select>
       </div>
       Modules selected: {modules.length}
-      <a href={buildUrl}>Build</a>
+      <a className={!downloading ? '' : 'downloading'} href={buildUrl} onClick={handleBuildClick}>
+        {!downloading ? 'Build' : <Loading />}
+      </a>
+      <a className="displaynone" tabIndex="-1" ref={aRef}></a>
     </nav>
   );
 }
